de05786509ed15a6d3938b05527fe037e878a415
ChrizTTt84 <christian.villegas.r@gmail.com>
2025-10-17 16:18:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -156,13 +156,13 @@ class SignatureGenerator {
                                     <table cellpadding="0" cellspacing="0" border="0" style="margin-top: 8px; margin-bottom: 8px;">
                                         <tr>
                                             <td style="padding-right: 8px;">
-                                                <img src="https://ChrizTTt84.github.io/Proyecto-Firmas-Digitales-Baca/Master%20Qualified%20Facility.png" alt="Master Qualified Facility Certified" style="max-width: 80px; max-height: 32px; object-fit: contain;">
+                                                <img src="https://ChrizTTt84.github.io/Proyecto-Firmas-Digitales-Baca/Master%20Qualified%20Facility.png" alt="Master Qualified Facility Certified" style="width: 60px !important; height: 24px !important; max-width: 60px !important; max-height: 24px !important; object-fit: contain !important; display: inline-block !important;">
                                             </td>
                                             <td style="padding-right: 8px;">
-                                                <img src="https://ChrizTTt84.github.io/Proyecto-Firmas-Digitales-Baca/9001_2015.png" alt="ISO 9001:2015 Certified" style="max-width: 80px; max-height: 32px; object-fit: contain;">
+                                                <img src="https://ChrizTTt84.github.io/Proyecto-Firmas-Digitales-Baca/9001_2015.png" alt="ISO 9001:2015 Certified" style="width: 60px !important; height: 24px !important; max-width: 60px !important; max-height: 24px !important; object-fit: contain !important; display: inline-block !important;">
                                             </td>
                                             <td style="padding-right: 8px;">
-                                                <img src="https://ChrizTTt84.github.io/Proyecto-Firmas-Digitales-Baca/fsc.png" alt="FSC Certified" style="max-width: 80px; max-height: 32px; object-fit: contain;">
+                                                <img src="https://ChrizTTt84.github.io/Proyecto-Firmas-Digitales-Baca/fsc.png" alt="FSC Certified" style="width: 60px !important; height: 24px !important; max-width: 60px !important; max-height: 24px !important; object-fit: contain !important; display: inline-block !important;">
                                             </td>
                                         </tr>
                                     </table>
